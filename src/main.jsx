76bc2a635dc8efa4c6b7f8220833a92d45e75f60
zahgenya <zahgenya@gmail.com>
2023-10-18 16:25:42 +0300

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+
+import { ApolloClient, InMemoryCache, gql, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>
 )
